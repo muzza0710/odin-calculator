09b7cedbea1007a852055aa8a481bacfddf07cb1
odin <142325575+muzza0710@users.noreply.github.com>
2024-12-09 14:46:26 +0000

    added function: basic functionallity
    plus, minus, multiply, divide and equals buttons all work, no order of operations implemented
    yet.

--- script.js
+++ script.js
@@ -1,2 +1,65 @@
-const digitButtons = document.querySelectorAll(".digits")
- 
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector('.equals')
+
+const screen = document.getElementById("screen");
+
+console.log(digitButtons);
+let firstNumber ;
+let secondNumber ;
+// let firstNumberIsLocked = false;
+// let secondNumberIsLocked = false;
+let oper = "";
+let enterSecondNumber = false;
+
+function calculate(a, b, operator ) {
+    switch (operator) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            if (b === 0) {
+                alert("cant divide by 0!!");
+                return a;
+            };
+            return a / b;
+
+    
+    }
+}
+
+equalsButton.addEventListener("click", () => {
+    console.log("=");
+    if (firstNumber && oper){
+        firstNumber = calculate(parseInt(firstNumber), parseInt(screen.value), oper);
+        screen.value = firstNumber;
+        oper = "";
+    }
+})
+
+digitButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (enterSecondNumber) {
+            screen.value = "";
+            enterSecondNumber = false;
+        }
+        screen.value += button.textContent;
+    })
+})
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (oper){
+            firstNumber = calculate(parseInt(firstNumber), parseInt(screen.value), oper)
+            console.log(firstNumber)
+            screen.value = firstNumber;
+        }
+        else {firstNumber = parseInt(screen.value);}
+        oper = button.textContent;
+        console.log(oper);
+        enterSecondNumber = true;
+    })
+})
