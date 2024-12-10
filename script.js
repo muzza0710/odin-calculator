7f9b68994edebc3b108ccc02444400f3bd023633
odin <142325575+muzza0710@users.noreply.github.com>
2024-12-10 13:47:26 +0000

    Added new buttons: period implemented
    implemented: period
    disabled: backspace and +/-.
    made some adjustments to = and operater buttons

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const digitButtons = document.querySelectorAll(".digit");
 const operatorButtons = document.querySelectorAll(".operator");
 const equalsButton = document.querySelector('.equals');
+
 const clearButton = document.querySelector('.clear');
+const posNegButton = document.querySelector(".pos-neg");
+const periodButton = document.querySelector(".period");
+const backspaceButton = document.querySelector(".backpace");
 
 const screen = document.getElementById("screen");
 const output = document.querySelector(".output");
 
-let firstNumber ;
+let firstNumber;
+let secondNumber;
 let oper = "";
+let enterFirstNumber = true;
 let enterSecondNumber = false;
 
 function calculate(a, b, operator ) {
@@ -31,25 +37,46 @@ function calculate(a, b, operator ) {
 
 clearButton.addEventListener("click", () => {
     firstNumber = "";
+    secondNumber =""
     oper = "";
     screen.value = "";
     output.textContent = "";
+    enterSecondNumber =false;
+    enterFirstNumber = true;
 })
 
 equalsButton.addEventListener("click", () => {
-    if (firstNumber){
-        firstNumber = calculate(parseInt(firstNumber), parseInt(screen.value), oper);
+    if (firstNumber && oper && screen.value){
+        secondNumber = parseFloat(screen.value);
+        output.textContent = firstNumber + oper + secondNumber;
+        firstNumber = calculate(parseFloat(firstNumber), secondNumber, oper);
         screen.value = firstNumber;
         oper = "";
-        output.textContent = firstNumber;
+        // firstNumber = "";
+        secondNumber = "";
+        enterSecondNumber = false;
+        enterFirstNumber = true;
     }
+})
+
+periodButton.addEventListener("click", () => {
+    if (!screen.value.includes(".") || !screen.value || !enterFirstNumber){
+        screen.value += ".";
+    } else if (enterSecondNumber || enterFirstNumber) {
+        screen.value = "";
+        enterSecondNumber = false;
+        enterFirstNumber = false;
+        screen.value += "0.";
+    }
+
 })
 
 digitButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        if (enterSecondNumber) {
+        if (enterSecondNumber || enterFirstNumber) {
             screen.value = "";
             enterSecondNumber = false;
+            enterFirstNumber = false;
         }
         screen.value += button.textContent;
     })
@@ -58,13 +85,14 @@ digitButtons.forEach((button) => {
 operatorButtons.forEach((button) => {
     button.addEventListener("click", () => {
         if (oper && screen.value){
-            firstNumber = calculate(parseInt(firstNumber), parseInt(screen.value), oper)
+            secondNumber = parseFloat(screen.value);
+            firstNumber = calculate(parseFloat(firstNumber), secondNumber, oper)
             screen.value = "";
             oper = "";
-        }
-        else if (screen.value){firstNumber = parseInt(screen.value);}
+        } else if (screen.value){firstNumber = parseFloat(screen.value);}
         oper = button.textContent;
         output.textContent = firstNumber + oper;
+        screen.value = "";
         enterSecondNumber = true;
     })
 })
